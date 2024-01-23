c3dce74bfaa4f641dd1cb681ccfff9df422eaa9b
91733 <91733@host.docker.internal>
2024-01-23 19:14:16 +0530

    first commit

---
+++ src/day1/ControlStm.java
@@ -0,0 +1,17 @@
+package day1;
+
+public class ControlStm {
+
+	static int i=50;
+	public static void main(String[] args) {
+		
+		if(i<100) {
+			System.out.println("good");
+		}
+		else {
+			System.out.println("bad");
+		}
+
+	}
+
+}
